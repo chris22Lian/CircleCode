f92dd0a47aa36599406d8333744fb9036aabdff5
Christoforos <53950987+chris22Lian@users.noreply.github.com>
2020-11-08 12:19:45 +0100

    code for Kandinsky

---
+++ sketch.js
@@ -0,0 +1,480 @@
+
+var lineColor = 0;
+
+function setup()
+{
+	
+	createCanvas(664, 682);
+	frameRate(100);
+	
+}
+
+function draw()
+{
+	 
+	background( 245, 245, 220 );
+	Stripe01();
+	Stripe02();
+	
+	innerCirles();
+	LargeCirle();
+	lines();
+	
+}
+
+function lines()
+{
+	line01();
+	line02();
+	line03();
+	line04();
+	line05();
+	line06();
+	line07();
+	line08();
+	line09();
+	line10();
+	line11();
+	line12();
+	line13();
+	line14();
+	line15();
+	line16();
+	line17();
+	line18();
+	line19();
+	line20();
+	line21();
+	line22();
+	line23();
+	line24();
+	line25();
+	line26();
+	line27();
+	line28();
+	line29();
+	line30();
+}
+
+function line30(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line( 400, 521, 437, 272 );
+}
+
+function line29(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line( 391, 530, 451, 492 );
+}
+
+function line28(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line( 388, 520, 445, 484 );
+}
+
+function line27(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line( 382, 511, 442, 475 );
+}
+
+function line26(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line( 178, 518, 227, 451);
+}
+
+function line25(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(191, 524, 250, 438);
+}
+
+function line24(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(196, 469, 283, 519);
+}
+
+function line23(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(166, 500, 291, 478);
+}
+
+function line22(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(127, 390, 519, 435);
+}
+
+
+function line21(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(132, 360, 452, 525);
+}
+
+function line20(){
+	strokeWeight(2);
+	stroke(lineColor);
+	line(92, 333, 553, 424);
+}
+
+
+function line19(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(176, 277, 165, 313);
+}
+
+function line18(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(483, 131, 416, 556);
+}
+
+function line17(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(99, 280, 528, 398);
+}
+
+function line16(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(138, 258, 107, 299);
+}
+
+function line15(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(126, 258, 100, 302);
+}
+
+function line14(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(101, 270, 528, 383);
+}
+
+function line13(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(471, 139, 407, 540);
+}
+
+function line12(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(465, 116, 221, 523);
+}
+
+function line11(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(307, 188, 125, 358);
+}
+
+function line10(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(367, 145, 156, 420);
+}
+
+function line09(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(483, 215, 486, 477);
+}
+
+function line08(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(478, 250, 512, 249);
+}
+
+function line07(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(476, 241, 504, 240);
+}
+
+function line06(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(476, 241, 504, 240);	
+}
+
+function line05(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(476, 241, 504, 240);	
+}
+
+function line04(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(202, 134, 544, 384);	
+}
+
+function line03(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(156, 174, 544, 239);	
+}
+
+function line02(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(337, 148, 364, 181);	
+}
+
+function line01(){
+	strokeWeight(1);
+	stroke(lineColor);
+	line(346, 136, 372, 172);	
+}
+
+function LargeCirle(){
+	strokeWeight(25);
+	stroke(0);
+	fill(26, 82, 118, 0);
+	ellipse(333, 328, 555);
+}
+
+function innerCirles(){
+
+	circle01();
+	circle02();
+	circle03();
+	circle04();
+	circle05();
+	circle06();
+	circle07();
+	circle08();
+	circle09();
+	
+	circle10();
+	circle11();
+	circle12();
+	circle13();
+	circle14();
+	circle15();
+	circle16();
+	circle17();
+	circle18();
+	circle19();
+	circle20();
+	circle21();
+	circle22();
+	circle23();
+	circle24();
+	circle25();
+
+}
+
+function circle25(){
+	strokeWeight(3);
+	stroke(26, 82, 118);
+	fill(26, 82, 118, 0);
+	ellipse(112, 381, 20);
+}
+
+function circle24(){
+	strokeWeight(3);
+	stroke(150, 0, 0);
+	fill(150, 0, 0, 0);
+	ellipse(153, 473, 35);
+}
+
+function circle23(){
+	strokeWeight(5);
+	stroke(0);
+	fill(255, 0, 0);
+	ellipse(144, 308, 30);
+}
+
+
+function circle22(){
+	strokeWeight(3);
+	stroke(0);
+	fill(0, 150, 0, 0);
+	ellipse(190, 388, 50);
+}
+
+function circle21(){
+	strokeWeight(3);
+	stroke(150, 0, 0);
+	fill(150, 0, 0, 0);
+	ellipse(244, 431, 60);
+}
+
+function circle20(){
+	strokeWeight(3);
+	stroke(0, 0, 0);
+	fill(0, 0, 0, 0);
+	ellipse(277, 474, 10);
+}
+
+function circle19(){
+	strokeWeight(3);
+	stroke(150, 0, 0);
+	fill(150, 0, 0, 0);
+	ellipse(320, 457, 15);
+}
+
+function circle18(){
+	strokeWeight(3);
+	stroke(100, 0, 0);
+	fill(100, 0, 0, 0);
+	ellipse(364, 532, 32);
+}
+
+function circle17(){
+	strokeWeight(3);
+	stroke(205, 205, 0);
+	fill(205, 205, 0, 0);
+	ellipse(392, 483, 20);
+}
+
+function circle16(){
+	strokeWeight(3);
+	stroke(139,0,139);
+	fill(139,0,139, 0);
+	ellipse(510, 470, 25);
+}
+
+function circle15(){
+	strokeWeight(3);
+	stroke(0, 0, 0);
+	fill(0, 0, 0, 0);
+	ellipse(447, 440, 25);
+}
+
+function circle14(){
+	strokeWeight(3);
+	stroke(180, 0, 0);
+	fill(180, 0, 0, 0);
+	ellipse(333, 391, 20);
+}
+
+function circle13(){
+	strokeWeight(3);
+	stroke(205, 205, 0);
+	fill(205, 205, 0, 0);
+	ellipse(420, 410, 100);
+}
+
+function circle12(){
+	strokeWeight(3);
+	stroke(0,50,0);
+	fill(0, 50, 0, 0);
+	ellipse(318, 353, 182);
+}
+
+function circle11(){
+	strokeWeight(3);
+	stroke(0,0,0);
+	fill(0, 0, 0, 0);
+	ellipse(308, 308, 18);
+}
+
+function circle10(){
+	strokeWeight(3);
+	stroke(255,165,0);
+	fill(255,165,0, 0);
+	ellipse(306, 307, 65);
+}
+
+function circle09(){
+	strokeWeight(3);
+	stroke(0, 80, 0);
+	fill(0, 80, 0, 0);
+	ellipse(258, 270, 67);
+}
+
+function circle08(){
+	strokeWeight(3);
+	stroke(180, 0, 0);
+	fill(180, 0, 0, 0);
+	ellipse(259, 205, 130);
+}
+
+function circle07(){
+	strokeWeight(3);
+	stroke(180, 0, 0);
+	fill(180, 0, 0, 0);
+	ellipse(340, 233, 100);
+}
+
+function circle06(){
+	strokeWeight(3);
+	stroke(180, 0, 0);
+	fill(180, 0, 0, 0);
+	ellipse(308, 94, 12);
+}
+
+function circle05(){
+	strokeWeight(3);
+	stroke(74, 5, 90);
+	fill(74, 5, 90, 0);
+	ellipse(450, 185, 75);
+}
+
+function circle04(){
+	strokeWeight(3);
+	stroke(0, 0, 0);
+	fill(0, 0, 0, 0);
+	ellipse(402, 217, 37);
+}
+
+function circle03(){
+	strokeWeight(3);
+	stroke(26, 82, 118);
+	fill(26, 82, 118, 0);
+	ellipse(505, 316, 52);
+}
+
+function circle02(){
+	strokeWeight(3);
+	stroke(0);
+	fill(0, 0, 0, 0);
+	ellipse(450, 270, 30);
+}
+function circle01(){
+	strokeWeight(3);
+	stroke(0,139,0);
+	fill(0,139,0, 0);
+	ellipse(442, 310, 150);
+}
+
+function Stripe01(){
+	
+	strokeWeight(5);
+	stroke(0, 110, 0);
+	fill(0, 110, 0);
+	beginShape();
+	vertex( 129, 9 );
+	vertex( 652, 645 );
+	vertex( 650, 398 );
+	vertex(228, 9);
+	endShape(CLOSE);
+	
+}
+function Stripe02(){
+	
+	strokeWeight(5);
+	stroke(155,155,53);
+	fill(155,155,53);
+	beginShape();
+	vertex( 606, 8 );
+	vertex( 653, 8 );
+	vertex( 56, 673 );
+	vertex( 8, 673 );
+	vertex( 12, 497 );
+	endShape(CLOSE);
+	
+}
